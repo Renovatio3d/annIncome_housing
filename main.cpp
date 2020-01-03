acd6bc4931fbca00fce0dfeba2988b7cc9a004e3
Shallu Nah <renovatio3d@gmail.com>
2020-01-02 18:39:15 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//AUTHOR: Shallu J Nah
+// PROGRAM DESCRIPTION: The program  compute the rent for an apartment in a low-income
+// housing unit, base on the annual income of the family, and the type of unit desired
+
+#include <iostream>            // for I/O, such ASCII
+#include <iomanip>            //  for formatting output
+#include <cmath>             //   for using pow/abs, such math functions
+#include <cctype>           //    Character handling functions, if any.
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
